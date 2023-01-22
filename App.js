a55c56bc83a1d28f450ba8c9069ce004a482abe1
espu <112239289+espu@users.noreply.github.com>
2023-01-22 21:56:44 +0200

    Actual first commit

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, Button, Alert, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1, flexDirection: 'column', alignItems: 'center', justifyContent: 'space-around'}}>
+      <Text>Hey look, a mysterious button!</Text>
+      <Button title="Button"onPress={buttonPressed}/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const buttonPressed = () => {  Alert.alert('Oh oh!');}
